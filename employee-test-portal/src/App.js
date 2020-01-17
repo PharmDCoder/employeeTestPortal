c6b7e34d4de6137a2fe3f8ecd11ce747c07347ad
Elizabeth Clohosey <Elizabethclohosey@gmail.com>
2020-01-16 21:01:27 -0500

    basic react set up and mock testset up.  Test set up includes mock test, mock test materials and mock PDF

---
+++ employee-test-portal/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+// import logo from './logo.svg';
+import "./App.css";
+import CarouselPage from "./components/Carousel/carousel.jsx";
+
+
+// **********  CAROUSEL  ************ 
+
+function App() {
+  return (
+    <div className="App">
+      <CarouselPage />
+    </div>
+  );
+}
+
+export default App;
