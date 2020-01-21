6ac89acb316171075cf14b64c678870d63f45c14
Elizabeth Clohosey <Elizabethclohosey@gmail.com>
2020-01-21 15:51:54 -0500

    tests are lightly styled and fucntioning

--- client/src/pages/Test/test.jsx
+++ client/src/pages/Test/test.jsx
@@ -107,7 +107,7 @@ const Test = ({ location, currentTests, user }) => {
 
   return (
     <MDBContainer className="carousel-container">
-      <h1 className="title">{Hipaa.title}</h1>
+      {/* <h1 className="title">{Hipaa.title}</h1> */}
       <MDBCarousel
         className="z-depth-1 carousel"
         activeItem={1}
