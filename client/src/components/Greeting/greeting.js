3dd18ec9ca9fcc0e16794cd4d94444e36aaa70e5
Elizabeth Clohosey <Elizabethclohosey@gmail.com>
2020-01-21 12:42:35 -0500

    fixed button issue on testList table.  Changed greeting from link to div

--- client/src/components/Greeting/greeting.js
+++ client/src/components/Greeting/greeting.js
@@ -1,18 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./greeting.css"
 
 const Greeting = ({ user }) => {
   return (
-    <Link className="greeting text-center" to="/">
+    <div className="text-center" to="/">
       {/* <p>{user && "SembraCare Portal - Hello " + user.employeeName}
         {!user && "SembraCare Portal"}</p> */}
-      <p>{user && "SembraCare Portal"}</p>
+      <p className="greeting">{user && "SembraCare Portal"}</p>
       <p className="greeting text-center">
         {user && "Hello " + user.employeeName + "!"}
       </p>
       <p>{!user && "SembraCare Portal"}</p>
-    </Link>
+    </div>
   );
 };
 
