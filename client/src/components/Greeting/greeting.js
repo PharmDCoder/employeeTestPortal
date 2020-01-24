e2dd4d9396929f0bb7a5d2d0d2b28cf1fe1158b0
Elizabeth Clohosey <Elizabethclohosey@gmail.com>
2020-01-24 18:04:57 -0500

    added blue gradient background to testList.  Removed SembraCare Portal from greeting for less clutter.  Changed color of greeting to look better with blue background

--- client/src/components/Greeting/greeting.js
+++ client/src/components/Greeting/greeting.js
@@ -6,7 +6,7 @@ const Greeting = ({ user }) => {
     <div className="greeting text-center" to="/">
       {/* <p>{user && "SembraCare Portal - Hello " + user.employeeName}
         {!user && "SembraCare Portal"}</p> */}
-      <h3 className="greeting">{user && "SembraCare Portal"}</h3>
+      {/* <h3 className="greeting">{user && "SembraCare Portal"}</h3> */}
       <h3 className="greeting text-center">
         {user && "Hello " + user.employeeName + "!"}
       </h3>
