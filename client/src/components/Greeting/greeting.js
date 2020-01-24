9adaafcd7cec968f69d073ead106d09e99f6f567
Elizabeth Clohosey <Elizabethclohosey@gmail.com>
2020-01-24 18:09:27 -0500

    changed font and size of testList greeting

--- client/src/components/Greeting/greeting.js
+++ client/src/components/Greeting/greeting.js
@@ -6,10 +6,10 @@ const Greeting = ({ user }) => {
     <div className="greeting text-center" to="/">
       {/* <p>{user && "SembraCare Portal - Hello " + user.employeeName}
         {!user && "SembraCare Portal"}</p> */}
-      {/* <h3 className="greeting">{user && "SembraCare Portal"}</h3> */}
-      <h3 className="greeting text-center">
+      {/* <h2 className="greeting">{user && "SembraCare Portal"}</h2> */}
+      <h2 className="greeting text-center">
         {user && "Hello " + user.employeeName + "!"}
-      </h3>
+      </h2>
       <h2>{!user && "SembraCare Portal"}</h2>
     </div>
   );
