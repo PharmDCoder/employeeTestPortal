67b68fd2aef28f6f83c3cf97ff49e5dac270b3b4
Jamie ONeill <joneill@sembracare.com>
2020-01-26 17:57:06 -0500

    server changes

--- server.js
+++ server.js
@@ -26,7 +26,11 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Make public a static folder
-app.use(express.static("public"));
+// app.use(express.static("public"));
+app.use(
+  "/static",
+  express.static(path.join(__dirname, "./client/build/index.html"))
+);
 
 app.use(routes);
 
