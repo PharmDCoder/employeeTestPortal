3f8212ef488da45e5451cb5af50a45c428805c82
Elizabeth Clohosey <Elizabethclohosey@gmail.com>
2020-01-26 16:01:20 -0500

    moved test title outside of row for style.  Test view is styled, still not responsive

--- client/src/pages/TestView/testView.jsx
+++ client/src/pages/TestView/testView.jsx
@@ -3,7 +3,7 @@ import "./testView.css";
 import Container from "../../components/Container";
 import Col from "../../components/Col";
 import Row from "../../components/Row";
-import Moment from "react-moment"
+import Moment from "react-moment";
 
 const TestView = ({ location, currentTests, user }) => {
   const [currentTest, setCurrentTest] = useState();
@@ -19,51 +19,72 @@ const TestView = ({ location, currentTests, user }) => {
       let currentTestRecord = user.testrecord.filter(test => {
         return test.testID === location.state.testid;
       });
-      setTestRecord(currentTestRecord[0])
+      setTestRecord(currentTestRecord[0]);
 
-      console.log(testLoad)
-      console.log(currentTestRecord)
-    } catch (ex) { }
+      console.log(testLoad);
+      console.log(currentTestRecord);
+    } catch (ex) {}
   });
 
   return (
     <Container className="summary-container">
+      {currentTest && <h2 className="text-center test-title">{currentTest.testName}</h2>}
       <Row className="text-center">
         {currentTest && (
-          <Col size="12"className="test-summary">
-            <h2 className="text-center">{currentTest.testName}</h2>
+          <Col size="12" className="test-summary">
             {/* <hr /> */}
             <h3 className="text-center">Test Score: {testRecord.testScore}%</h3>
-            <h3>Completed On: {" "}
-              <Moment format="MM/DD/YY">
-                {testRecord.testFinish}
-              </Moment>
+            <h3>
+              Completed On:{" "}
+              <Moment format="MM/DD/YY">{testRecord.testFinish}</Moment>
             </h3>
-            <h3>Total Test Time: {" "}
+            <h3>
+              Total Test Time:{" "}
               <Moment diff={testRecord.testStart} unit="m">
                 {testRecord.testFinish}
-              </Moment>
-              {" "} minutes
+              </Moment>{" "}
+              minutes
             </h3>
-            <img src={testRecord.testSignature} alt="signature" className="bg-white signature-image"/>
+            <img
+              src={testRecord.testSignature}
+              alt="signature"
+              className="bg-white signature-image"
+            />
             {/* <hr /> */}
           </Col>
         )}
       </Row>
       {testRecord && (
-        <Row  className="text-center">>
+        <Row className="text-center">
+          >
           <Col size="12" className="test-answer-explanations">
-            {testRecord.testQuestionList.map((testQuestion,index) => {
+            {testRecord.testQuestionList.map((testQuestion, index) => {
               console.log(testQuestion);
               return (
-                <div id="question-explanation" className={!testQuestion.testQuestionCorrect ? "text-danger" : "text-dark"
-                 }>
-                  <p>{index+1} - {testQuestion.testQuestionText}</p>
-                  <p><strong>Correct Answer:</strong> {testQuestion.testQuestionAnswer}</p>
-                  {!testQuestion.testQuestionCorrect && (<p><strong>Explanation: </strong>{testQuestion.testQuestionExplanation}</p>)}
+                <div
+                  id="question-explanation"
+                  className={
+                    !testQuestion.testQuestionCorrect
+                      ? "text-danger"
+                      : "text-dark"
+                  }
+                >
+                  <p>
+                    {index + 1} - {testQuestion.testQuestionText}
+                  </p>
+                  <p>
+                    <strong>Correct Answer:</strong>{" "}
+                    {testQuestion.testQuestionAnswer}
+                  </p>
+                  {!testQuestion.testQuestionCorrect && (
+                    <p>
+                      <strong>Explanation: </strong>
+                      {testQuestion.testQuestionExplanation}
+                    </p>
+                  )}
                   {/* <hr /> */}
                 </div>
-              )
+              );
             })}
           </Col>
         </Row>
